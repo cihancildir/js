371f586b98d30e462f21902f3185d11debe571f6
cihancildir <cihancildir@gmail.com>
2021-10-19 00:29:23 +0300

    master

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log('Merhaba Dunya')
+
+let dolarDun=9.20
+let dolarBugun=90.30
+const euroDun=11
+//let euroDun = 11.5
+// if (dolarDun<dolarBugun){
+ //console.log('dolar Düşmüştür.')
+// }
+ let konutKredileri = ["1.Kredi","2.Kredi Türü","3.Kredi Türü"]
+for(let i=0;i<konutKredileri.length;i++){
+console.log(konutKredileri[i])
+
+}
